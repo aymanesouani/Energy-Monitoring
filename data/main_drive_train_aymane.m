aca81e992d949788f79ce0a25706719e43f0decb
aymanesouani <52032789+aymanesouani@users.noreply.github.com>
2020-01-10 12:17:49 +0100

    Add files via upload
    Last update Friday 10 january

--- data/main_drive_train_aymane.m
+++ data/main_drive_train_aymane.m
@@ -124,24 +124,112 @@ pwelch(spwm, window)
 
 %% Simu de coherence spectrale
 
-t = 0:1/1e3:2;
-signal_entree = chirp(t,0,1,250);
-
+ipwm_carre = ipwm.^2;
 figure(1)
-plot(signal_entree)
-title('Signal entrÃ©e : Chirp')
+subplot(3,1,1)
+coherence_icarre_vibration = mscohere(ipwm_carre, vpwm, window, [],[], fs);
+plot(coherence_icarre_vibration)
+hold on
+plot(mscohere(ipwm, vpwm, window, [],[], fs))
+title('Coherence spectrale i^2 /vibrations')
 
-windowSize = 2; 
-b = (1/windowSize)*ones(1,windowSize);
-a = 1;
-x = filter(b,a,signal_entree);
 
+subplot(3,1,2)
+coherence_i_son = mscohere(ipwm, spwm, window, [],[], fs);
+plot(coherence_i_son)
+title('Coherence spectrale i/son')
+
+subplot(3,1,3)
+coherence_son_vibration = mscohere(spwm, vpwm, window, [],[], fs);
+plot(coherence_son_vibration)
+title('Coherence spectrale son/vibrations')
+
+
+
+%% Estimation de vibrations avec i_carre
+Ndiv = 2560;
+fenetre = hanning(Ndiv);
+Cii = cpsd(ipwm_carre, ipwm_carre,fenetre , [], [], fs);
+Civ = cpsd(vpwm, ipwm_carre, fenetre, [], [], fs);
+% Filtre
+H_iv = Civ ./ Cii ; 
+figure(1)
+subplot(1,2,1)
+plot(abs(H_iv))
+title("Module fréquentiel du filtre")
+subplot(1,2,2)
+plot(180*angle(H_iv)/pi)
+title("Argument(H(f)) ")
+
+% Reconstruction signal temporel:
+ifft_H_iv = ifft(H_iv);
 figure(2)
-plot(x)
-noise = wgn(,power);
-signal_bruite =  signal_entree + noise ;
+plot(abs(ifft_H_iv))
+
+% Estimation : 
+
+Vpwm_estime = conv(ipwm_carre, real(ifft_H_iv));
 figure(3)
-plot(signal_bruite)
+subplot(2,1,1)
+plot(vpwm)
+title("Vibrations réelles")
+subplot(2,1,2)
+plot(Vpwm_estime)
+title("Vibrations estimées")
+
+
+Cvv = cpsd(Vpwm_estime, Vpwm_estime,fenetre , [], [], fs);
+Csv = cpsd(spwm, Vpwm_estime(1:256000), fenetre, [], [], fs);
+% Filtre
+H_sv = Csv ./ Cvv ; 
+figure(4)
+subplot(1,2,1)
+plot(abs(H_sv))
+title("Module fréquentiel du filtre")
+subplot(1,2,2)
+plot(180*angle(H_sv)/pi)
+title("Argument(H(f)) ")
+
+% Reconstruction signal temporel:
+ifft_H_sv = ifft(H_sv);
+figure(5)
+plot(abs(ifft_H_sv))
+
+% Estimation : 
+
+Spwm_estime = conv(Vpwm_estime, real(ifft_H_sv));
+figure(6)
+subplot(2,1,1)
+plot(spwm)
+title("Son réel")
+subplot(2,1,2)
+plot(Spwm_estime)
+title("Son estimé")
+
+
+% Plot des DSP :
+figure(7)
+subplot(2,1,1)
+plot(pwelch(vpwm, fenetre))
+hold on
+plot(pwelch(Vpwm_estime, fenetre))
+
+subplot(2,1,2)
+plot(pwelch(spwm, fenetre))
+hold on
+plot(pwelch(Spwm_estime, fenetre))
+
+figure(8)
+subplot(2,1,1)
+plot(pwelch(vpwm, fenetre))
+hold on
+plot(pwelch(Vpwm_estime, fenetre))
+
+subplot(2,1,2)
+plot(pwelch(spwm, fenetre))
+hold on
+plot(pwelch(Spwm_estime, fenetre))
+
 
 
 
